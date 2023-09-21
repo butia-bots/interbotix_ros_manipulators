743f2962a3feebbc56be0a57483144b51fab5d4e
edompedro <pedroedomnunes@gmail.com>
2023-09-21 17:35:47 -0300

    tava no robo

--- interbotix_ros_xsarms/examples/python_demos/bartender.py
+++ interbotix_ros_xsarms/examples/python_demos/bartender.py
@@ -7,22 +7,10 @@ import numpy as np
 # Then change to this directory and type 'python bartender.py'
 
 def main():
-    bot = InterbotixManipulatorXS("wx250", "arm", "gripper")
-    bot.arm.set_ee_pose_components(x=0.3, z=0.2)
-    bot.arm.set_single_joint_position("waist", np.pi/2.0)
+    bot = InterbotixManipulatorXS("doris_arm", "arm", "gripper")
     bot.gripper.open()
-    bot.arm.set_ee_cartesian_trajectory(x=0.1, z=-0.16)
     bot.gripper.close()
-    bot.arm.set_ee_cartesian_trajectory(x=-0.1, z=0.16)
-    bot.arm.set_single_joint_position("waist", -np.pi/2.0)
-    bot.arm.set_ee_cartesian_trajectory(pitch=1.5)
-    bot.arm.set_ee_cartesian_trajectory(pitch=-1.5)
-    bot.arm.set_single_joint_position("waist", np.pi/2.0)
-    bot.arm.set_ee_cartesian_trajectory(x=0.1, z=-0.16)
     bot.gripper.open()
-    bot.arm.set_ee_cartesian_trajectory(x=-0.1, z=0.16)
-    bot.arm.go_to_home_pose()
-    bot.arm.go_to_sleep_pose()
 
 if __name__=='__main__':
     main()
